2e01ff7f555552fcb093deace044cdc8b808eec4
Vichito <ignaciofigue9@gmail.com>
2025-10-15 21:43:48 -0300

    ej 2 parte 1 // creacion de la carpeta math-string

--- estructuras/estructuras-p1/estruc-p1/Ejercicios.java
+++ estructuras/estructuras-p1/estruc-p1/Ejercicios.java
@@ -1,17 +1,39 @@
+//clase en la que vamos a realizar los ejercicios
 
-import java.util.Scanner;
+import java.util.Scanner; //necesitamos incluir la clase Scanner para poder leer datos del teclado
 
 public class Ejercicios{
-    private Scanner leer=new Scanner(System.in);
+    private Scanner leer=new Scanner(System.in); //definimos nuestra variable Scanner que usaremos en toda la clase
+    
     public void ejercicio1(){
          int suma=0;
-        for(int i=0;i<10;i++){
+        for(int i=0;i<10;i++){//leemos los 10 numeros con el for
                 System.out.print("introduzca " + (i+1) + " numero: ");
-                suma=suma + leer.nextInt();
-            
-            
+                suma=suma + leer.nextInt();//hacemos que la suma sea igual a la suma + el numero ingresado
         }
         System.out.println("la suma total es: " + suma);
+    }
+    
+    public void ejercicio2(){
+        int suma=0;
+        int contadorDe20s=0;
+            System.out.print("introduzca un numero: ");
+        int numero = leer.nextInt();
+        while(numero!=0){
+            suma=suma + numero;
+            if (numero==20){contadorDe20s ++;}//si el numero es igual a 20 icrementamos en 1 el contador de 20s
+            System.out.print("introduzca un numero: ");
+            numero = leer.nextInt();//volvemos a leer para que se repita el ciclo
+         
+        };
+        
+        System.out.println("la suma total es: " + suma);
+        System.out.println("El numero 20 se ingreso un total de : " + contadorDe20s + " veces");
+        
+        if(suma>100){
+            System.out.println("SUPERA LOS $100");
+        }
+        
     }
     
 }
--- estructuras/estructuras-p1/estruc-p1/Principal.java
+++ estructuras/estructuras-p1/estruc-p1/Principal.java
@@ -1,13 +1,21 @@
 
 //actividades del pdf estructuras parte 1
 
+
+//esta es la clase principal (Main) donde se va a inicializar, es decir, dar uso, a todas las clases, metodos, etc
+//que vayamos creando en el proyecto entero.
 public class Principal{
 
     public static void main(){
-    
+        
+        //llamamos a nuestra clase Ejercicios, donde ya estan definidos los metodos. cada metodo es un ejercicio 
+        //distinto de la parte 1 de el pdf "estructuras".
         Ejercicios ej=new Ejercicios();
         
-        ej.ejercicio1();
+        ej.ejercicio1();//llamamos al primer metodo de la clase, es decir, el ejercicio 1.
+        //ahora hacemos lo mismo con los demas ejercicios
+        ej.ejercicio2();
+        //ej.ejercicio3();
     
     }
 
